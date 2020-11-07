27dec36ecece9fd04c2c3a708f7a660a592428d7
Артем Черенков <art.cherenkov@gmail.com>
2020-11-08 02:31:08 +0700

    Рефакторит index.js:
    Подключен Redux, настроено создание хранилища и активирована поддержка Redux Dev Tools. + теперь главный компонент - App

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-import Table from "./components/table/table.jsx";
+import rootReducer from './store/reducers/root-reducer';
 
-const root = document.getElementById(`root`);
+import App from "./components/app/app";
+
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
-    <Table />,
-    root
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById(`root`)
 );
