f3ad82468ec1014ec294604625e9c591f78f5f50
Артем Черенков <art.cherenkov@gmail.com>
2020-11-08 02:29:13 +0700

    Рефакторит компонент Table:
    Переводит его в функциональный компонент, добавляет хуки useDispatch и useSelector для связи с хранилищем, реализует тестовое добавление новой расчетной секции.

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+
 import {HEADERS, SectionTypes} from '../../const.js';
 
 import HeaderSection from "../headers-section/headers-section.jsx";
 import Section from "../section/section.jsx";
+import {useDispatch, useSelector} from "react-redux";
+import {addSection as addSectionAction} from "../../store/action";
+import {getSectionsCount} from "../../store/reducers/app-state/selectors";
+import {nanoid} from "nanoid";
 
-class Table extends React.PureComponent {
-  render() {
-    return <div className="table">
-      <HeaderSection headers={HEADERS} />
-      <Section type={SectionTypes.MAIN} />
-    </div>;
-  }
-}
+const Table = () => {
+  const dispatch = useDispatch();
+
+  const sectionsCount = useSelector((state) => getSectionsCount(state));
+
+  const addSection = useCallback(
+      () => dispatch(addSectionAction()),
+      [dispatch]
+  );
+
+  const getSections = (count) => {
+    let sections = [];
+    for (let i = 0; i < count; i++) {
+      sections.push(SectionTypes.MAIN);
+    }
+
+    return sections;
+  };
+
+  return <div className="table">
+    <button onClick={addSection}>Click me</button>
+    <HeaderSection headers={HEADERS} />
+    {getSections(sectionsCount).map((type) => <Section key={nanoid()} type={type} />)}
+  </div>;
+};
 
 export default Table;
